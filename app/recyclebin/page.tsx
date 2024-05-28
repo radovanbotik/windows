23dd675dc42af18ff9635759b698a9dd143b0e16
radovanbotik <radovanbotik@gmail.com>
2024-05-28 14:46:45 +0200

    add body to recycle bind remove icons

--- app/recyclebin/page.tsx
+++ app/recyclebin/page.tsx
@@ -6,18 +6,18 @@ import Icon from "../UI/Icon";
 import MenuDropdown from "../UI/MenuDropdown";
 import Button from "../UI/Button";
 
-const icons = [
-  {
-    name: "University Degree",
-    href: "#",
-    icon: directory,
-  },
-  {
-    name: "About",
-    href: "#",
-    icon: file,
-  },
-];
+// const icons = [
+//   {
+//     name: "University Degree",
+//     href: "#",
+//     icon: directory,
+//   },
+//   {
+//     name: "About",
+//     href: "#",
+//     icon: file,
+//   },
+// ];
 
 export default function page() {
   return (
@@ -42,16 +42,16 @@ export default function page() {
             </Button>
           </MenuDropdown>
         </Window.Toolbar>
-        <Window.Body className="p-2">
-          <div className="w-full h-full flex  gap-4 flex-wrap content-start px-2">
+        <Window.Body className="bg-windows-white p-2 border-b-windows-gray border-r-windows-gray border-r-2 border-b-2 shadow-[inset_2px_2px_0px_0px_#000]">
+          {/* <div className="w-full h-full flex  gap-4 flex-wrap content-start px-2">
             {icons.map((icon, index) => {
               return <Icon key={icon.name} {...icon} />;
             })}
-          </div>
+          </div> */}
         </Window.Body>
         <Window.Footer className="flex gap-1">
           <div className="w-2/3 h-6 mt-1 px-2 bg-windows-gray border-b-windows-white border-r-windows-white border-r-2 border-b-2 shadow-[inset_2px_2px_0px_0px_#8E888E]">
-            <span className="text-sm">{icons.length} object(s)</span>
+            {/* <span className="text-sm">{icons.length} object(s)</span> */}
           </div>
           <div className="w-1/3 h-6 mt-1 px-2 bg-windows-gray border-b-windows-white border-r-windows-white border-r-2 border-b-2 shadow-[inset_2px_2px_0px_0px_#8E888E]"></div>
         </Window.Footer>
